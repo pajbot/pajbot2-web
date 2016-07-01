a758d3cb84dd5ac69ab468d03336370dc5059f75
Rasmus Karlsson <pajlada@bithack.se>
2016-07-01 00:00:10 +0000

    experimenting with angular

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,32 @@ function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
   $stateProvider
   .state('main', {
     url: '/',
-    template: '<h1>xD </h1>'
-  });
+    templateUrl: 'main/main.html',
+    views: {
+        'sideNav': {
+            template: 'MENU',
+        },
+    }
+  })
+  .state('dashboard', {
+    url: '/dashboard',
+    views: {
+        'sideNav': {
+            template: 'MENU',
+        },
+        '': {
+            templateUrl: 'dashboard/dashboard.html',
+        },
+    },
+    abstract: true
+  })
+  .state('dashboard.main', {
+    url: '',
+    templateUrl: 'dashboard/main/main.html',
+  })
+  .state('dashboard.test', {
+    url: '/test',
+    template: '<h1>dashboard test</h1>',
+  })
+  ;
 }
